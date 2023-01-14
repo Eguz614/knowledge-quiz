60122db9151250af1a2676eb79b938e96ab3e09f
Eguz614 <ericguz614@gmail.com>
2023-01-13 23:45:23 -0800

    added start button hide and questions card to appear

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+var startButton = document.querySelector('#start-btn');
+var questionCard = document.querySelector('#question-card');
+
+
+ startButton.addEventListener('click', startQuiz)
+
+
+function startQuiz(){
+    console.log("working")
+    startButton.classList.add("hide")
+    questionCard.classList.remove("hide")
+
+
+}
+
+function nextQuestion(){
+
+
+}
+
+function selection(){
+
+
+}
